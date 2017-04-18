f133a1e76cb6faf8bde7cdc893a5bfccdd4afa64
KyoungRan K <kyoungrankang@gmail.com>
2017-04-18 23:01:24 +0900

    NoMatch404

---
+++ 06-nomatch404/src/containers/routes/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <p>
+            A
+            <code> &lt;Switch&gt; </code>
+            renders the first child
+            <code> &lt;Route&gt; </code>
+            that matches. A
+            <code> &lt;Route&gt; </code>
+            with no
+            <code> path </code>
+            always matches.
+        </p>
+    );
+};
+
+export default Home;
---
+++ 06-nomatch404/src/containers/routes/WillMatch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WillMatch = () => {
+    return (
+        <div>
+            <h3>Matched!</h3>
+        </div>
+    );
+};
+
+export default WillMatch;
